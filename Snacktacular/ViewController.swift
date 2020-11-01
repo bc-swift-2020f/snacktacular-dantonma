139064d057fc0f8bfdd9f19dd70fcf6792fcef99
Marissa D'Antonio <dantonma@bc.edu>
2020-11-01 17:39:12 -0500

    Initial Commit

---
+++ Snacktacular/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snacktacular
+//
+//  Created by Marissa D'Antonio on 11/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
